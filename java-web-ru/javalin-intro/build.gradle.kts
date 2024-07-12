4814e21ab920e5df799693c4821bb8d96453dfed
@hexlet/cli <support@hexlet.io>
2024-07-12 10:41:58 +0300

    submit java-web-ru/javalin-intro

--- java-web-ru/javalin-intro/build.gradle.kts
+++ java-web-ru/javalin-intro/build.gradle.kts
@@ -20,7 +20,8 @@ repositories {
 
 dependencies {
     // BEGIN
-    
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
     // END
     implementation("org.slf4j:slf4j-simple:2.0.7")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
