ed22c3334fc9c506bf4d97331572bcb6cd8c5d81
@hexlet/cli <support@hexlet.io>
2024-08-03 11:35:30 +0300

    submit java-spring-ru/dependency-injection

--- java-spring-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
@@ -1,17 +1,17 @@
 package exercise.controller;
 
-
+import exercise.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
-
+import org.springframework.http.HttpStatus;
 import java.util.List;
 
 import exercise.model.Post;
@@ -25,6 +25,8 @@ public class PostsController {
 
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
     @GetMapping(path = "")
     public List<Post> index() {
@@ -33,12 +35,12 @@ public class PostsController {
 
     @GetMapping(path = "/{id}")
     public Post show(@PathVariable long id) {
-        var post = postRepository.findById(id)
+        return postRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("post with id " + id + " not found"));
-        return post;
     }
 
     @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
     public Post create(@RequestBody Post post) {
         if (postRepository.findAll().contains(post)) {
             throw new ResourceNotFoundException("Post " + post.getTitle() + " already exists");
@@ -62,6 +64,7 @@ public class PostsController {
     @DeleteMapping(path = "/{id}")
     public void delete(@PathVariable long id) {
         postRepository.deleteById(id);
+        commentRepository.deleteByPostId(id);
     }
 }
 // END
